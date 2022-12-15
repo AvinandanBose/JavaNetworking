b78db64edf32d422a226988797d2d8903122dbdb
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-15 22:33:43 +0530

    Update Socket[Connecting Address Through a Port]-Constructor2.java

--- Socket[Connecting Address Through a Port]-Constructor2.java
+++ Socket[Connecting Address Through a Port]-Constructor2.java
@@ -1,18 +1,32 @@
-import java.net.Socket;
-import java.net.InetAddress;
-class Socket3{
-    static Socket s;
-     
-    public static void main(String[] args) {
-        try {
-            InetAddress localAdress = InetAddress.getLocalHost();
-            
-            s = new Socket("www.google.com", 80, localAdress, 8080);
-            System.out.println("Socket created");
-           
 
-        } catch (Exception e) {
-            System.out.println("I/O error " + e);
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+
+public class SocketNew {
+    static Socket socket ;
+    public static void main(String[] args)  {
+
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+            InetAddress localAdress = InetAddress.getLocalHost();
+            System.out.println(address.getHostAddress());
+            System.out.println(address.getHostName());
+            
+            socket = new Socket(address.getHostName(),80,  localAdress, 8080);
+            System.out.println(socket.getInetAddress());
+            System.out.println(socket.getPort());
+           
+           
+        }catch(UnknownHostException e){
+            System.out.println("Unknown Host");
+        } catch (IOException e) {
+            
+            e.printStackTrace();
         }
+
+
     }
+    
 }
