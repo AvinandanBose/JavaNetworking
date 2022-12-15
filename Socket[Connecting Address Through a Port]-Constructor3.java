e2081f9a9e127a829b836565f21ae906f779ff30
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-15 22:11:51 +0530

    Update Socket[Connecting Address Through a Port]-Constructor3.java

--- Socket[Connecting Address Through a Port]-Constructor3.java
+++ Socket[Connecting Address Through a Port]-Constructor3.java
@@ -1,16 +1,27 @@
 
+
+
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.InetAddress;
 
-class Socket4 {
+public class SocketNew {
 
-    public static void main(String[] args) throws Exception {
-        Socket s = new Socket( ); 
-        s.connect( new java.net.InetSocketAddress("www.google.com", 80));
-        System.out.println("Socket is connected: " + s.isConnected());
-        System.out.println("Socket is closed: " + s.isClosed());
-        s.close();
-        System.out.println("Socket is connected: " + s.isConnected());
-        System.out.println("Socket is closed: " + s.isClosed());
-    }
-    
-}
+    public static void main(String[] args)  {
+
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+            System.out.println(address.getHostAddress());
+            System.out.println(address.getHostName());
+            Socket socket = new Socket();
+            socket.connect(new java.net.InetSocketAddress(address.getHostAddress(), 80));
+            System.out.println(socket.getInetAddress());
+            System.out.println(socket.getPort());
+           
+        }catch(UnknownHostException e){
+            System.out.println("Unknown Host");
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
