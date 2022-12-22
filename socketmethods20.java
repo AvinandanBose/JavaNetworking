3a7ca758991755e2cae49c2446d835d075455485
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-22 23:01:49 +0530

    Add files via upload

--- socketmethods20.java
+++ socketmethods20.java
@@ -1,3 +1,39 @@
+package SocketMethods;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 public class socketmethods20 {
+
+    static Socket s;
+    static Socket s1;
+    
+    public static void main(String[] args) {
+        try {
+            s = new Socket("www.google.com", 80);
+            System.out.println("Is Connected: " + s.isConnected());
+            System.out.println("Timeout: " + s.getSoTimeout());
+            s.close();
+
+            // Type2
+
+            InetAddress inetAddress = InetAddress.getByName("www.google.com");
+            int port = 80;
+            String localHostName = inetAddress.getHostName();
+            SocketAddress socketAddress = new InetSocketAddress(localHostName, port);
+            s1 = new Socket();
+            s1.connect(socketAddress);
+            System.out.println("Timeout: " + s1.getSoTimeout());
+
+            s1.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
     
 }
