a8aecdf608b5416e674a0786928d88d50b8f7d4e
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-30 23:04:18 +0530

    Add files via upload

--- socketmethods22.java
+++ socketmethods22.java
@@ -1,3 +1,37 @@
+package SocketMethods;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 public class socketmethods22 {
+    static Socket s;
+    static Socket s1;
     
+    public static void main(String[] args) {
+        try {
+            s = new Socket("www.google.com", 80);
+            System.out.println("Is Connected: " + s.isConnected());
+            System.out.println("Get Taffic Class: " + s.getTrafficClass());
+            s.close();
+
+            // Type2
+
+            InetAddress inetAddress = InetAddress.getByName("www.google.com");
+            int port = 80;
+            String localHostName = inetAddress.getHostName();
+            SocketAddress socketAddress = new InetSocketAddress(localHostName, port);
+            s1 = new Socket();
+            s1.connect(socketAddress);
+            System.out.println("Get Taffic Class: " + s1.getTrafficClass());
+
+            s1.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
